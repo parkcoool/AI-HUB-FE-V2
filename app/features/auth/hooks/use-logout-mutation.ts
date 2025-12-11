f41a6e87e6e36d863115cbe79ac161a62e58f2c4
parkcoool <yickle1345@naver.com>
2025-12-11 19:40:12 +0900

    Fix(auth): `useLogoutMutation`의 `mutationFn`이 success로 처리되도록 수정

--- app/features/auth/hooks/use-logout-mutation.ts
+++ app/features/auth/hooks/use-logout-mutation.ts
@@ -1,6 +1,8 @@
 import { useMutation } from "@tanstack/react-query";
 
 import { api } from "~/lib/api";
+import { AppError } from "~/lib/error";
+import { AppErrorCodeSchema } from "~/types/error-code";
 
 type LogoutResponse = void;
 
@@ -8,8 +10,19 @@ export function useLogoutMutation() {
   return useMutation<LogoutResponse>({
     mutationKey: ["logout"],
     mutationFn: async () => {
-      const response = await api.post<LogoutResponse>("/auth/logout");
-      return response.data;
+      try {
+        const response = await api.post<LogoutResponse>("/auth/logout");
+        return response.data;
+      } catch (error) {
+        // /auth/logout 엔드포인트는 204 No Content를 반환
+        if (
+          error instanceof AppError &&
+          error.code === AppErrorCodeSchema.enum.INVALID_RESPONSE_FORMAT
+        ) {
+          return;
+        }
+        throw error;
+      }
     },
     onSuccess: async (_data, _variables, _onMutateResult, context) => {
       context.client.invalidateQueries({ queryKey: ["get-user"] });
